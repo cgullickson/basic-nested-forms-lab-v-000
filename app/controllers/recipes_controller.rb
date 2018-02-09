02abbb2dd0744542f3127388300fe6cdae135287
Chris Gullickson <cgullickson5@gmail.com>
2018-02-09 00:26:50 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,4 +21,17 @@ class RecipesController < ApplicationController
     end
   end
 
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes: [
+      :name,
+      :quantity
+    ]
+
+
+    )
+
 end
